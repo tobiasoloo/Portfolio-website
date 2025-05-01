dc581d225ac376a5f22b25b0dd70b166bd2f8417
Tobias <145672462+tobiasoloo@users.noreply.github.com>
2025-05-01 11:01:23 +0300

    Add files via upload

---
+++ Tobias.shop/birthday wishes.py
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Table Assignment 1 </title>
+		<meta charset ="utf-8">
+	</head>
+	<body>
+		<table border ="1" text ="center">
+			<h1> ASSIGNMENT 1 </h1>
+			<center>
+			<tr>
+				<th colspan ="6" align ="center"> STUDENTS MARK SHEET </th>
+			</tr>
+			</center>
+			<tr>
+				<th> Name </th>
+				<th> Maths </th>
+				<th> Science </th>
+				<th> English </th>
+				<th> Physics </th>
+				<th> General Knowledge </th>
+			</tr>
+			<tr>
+				<td> David </td>
+				<td> 85 </td>
+				<td> 87 </td>
+				<td> 88 </td>
+				<td> 92 </td>
+				<td> 88 </td>
+			</tr>
+			<tr>
+				<td> Richard </td>
+				<td> 91 </td>
+				<td> 81 </td>
+				<td> 78 </td>
+				<td> 71 </td>
+				<td> 74 </td>
